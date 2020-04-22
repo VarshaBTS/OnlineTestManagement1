d4f74c0c8e345003f672d8b965e9f2449191400e
varsha <vmusham@gmail.com>
2020-04-22 12:04:01 +0530

    second commit

--- OnlineTestManagementSystem/src/main/java/com/capgemini/onlinetest/controller/UserController.java
+++ OnlineTestManagementSystem/src/main/java/com/capgemini/onlinetest/controller/UserController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.capgemini.onlinetest.entity.Userdata;
-//
+
 @RestController
 @RequestMapping("/user")
 @CrossOrigin("http://localhost:4200")
